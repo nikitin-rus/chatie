f2844acd8cc465080205a2e82130646416a4837f
Nikitin Ruslan <mystudychannel.0@gmail.com>
2024-09-09 22:59:56 +0300

    feat: add links

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { Container, Flex, Heading, IconButton } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  textDecoration,
+} from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import { Close } from "./icons/Close";
+import NextLink from "next/link";
+import paths from "@/paths";
 
 export function Header() {
   const { t } = useTranslation();
+
+  function handleClose() {}
 
   return (
     <Container
@@ -22,16 +33,28 @@ export function Header() {
         left={0}
         right={0}
       >
-        <Heading textStyle="h1" as="h1">
-          {t("pages.main.chatie")}
-        </Heading>
+        <Link
+          as={NextLink}
+          href={paths.root}
+          cursor="pointer"
+          color="grayscale.7"
+          _hover={{
+            textDecoration: "none",
+          }}
+        >
+          <Heading textStyle="h1" as="h1">
+            {t("pages.main.chatie")}
+          </Heading>
+        </Link>
 
-        <IconButton
-          size="iconDefault"
-          aria-label="Close auth popup"
-          icon={<Close />}
-          isRound={true}
-        />
+        <Link href={paths.root}>
+          <IconButton
+            size="iconDefault"
+            aria-label="Close auth popup"
+            icon={<Close />}
+            isRound={true}
+          />
+        </Link>
       </Flex>
     </Container>
   );
